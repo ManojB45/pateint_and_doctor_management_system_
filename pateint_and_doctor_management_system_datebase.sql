330347edec8521493e38092b730070998a7a0403
ManojB45 <manojb1998225@gmail.com>
2025-01-31 07:01:01 +0530

    first commit

---
+++ pateint_and_doctor_management_system_datebase.sql
@@ -0,0 +1,7 @@
+create database Pateint_and_Doctor_Management_System_Datebase;
+use Pateint_and_Doctor_Management_System_Datebase;
+
+select * from appointments;
+select * from doctors;
+select * from medications;
+select * from patients;
